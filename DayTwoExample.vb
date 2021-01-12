25af1902d1799dbbed68735d409f33098694ba7d
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2021-01-12 08:48:01 -0700

    Add project files.

---
+++ DayTwoExample.vb
@@ -0,0 +1,17 @@
+﻿Module DayTwoExample
+
+    Sub Main()
+        Console.WriteLine("Some String")
+        Console.WriteLine("Some String")
+        Console.WriteLine("Some String")
+        Console.WriteLine("Some String")
+        Console.Beep(392, 500) 'G4'
+        Console.Beep(440, 500) 'A4'
+        Console.Beep(349, 750) 'F4'
+        Console.Beep(196, 750) 'G3'
+        Console.Beep(262, 900) 'C4'
+        Console.ReadLine()
+
+    End Sub
+
+End Module
